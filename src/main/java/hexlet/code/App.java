a8ab550a460a7cb11a7342866b41ea4b3b4551bb
VIIIunknownVIII <hussarrodion@gmail.com>
2024-04-14 18:31:25 +0700

    1

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,14 @@
 package hexlet.code;
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
  class App {
     public static void main(String[] args) {
+        String user;
         System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        user = Cli.user();
+        System.out.println("Hello, " + user+ "!");
     }
 }
---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static String user(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+}

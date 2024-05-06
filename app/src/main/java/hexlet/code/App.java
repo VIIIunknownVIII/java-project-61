798785ae1072bec9a40976f93f19f3c0d607b067
VIIIunknownVIII <hussarrodion@gmail.com>
2024-05-06 17:21:56 +0700

    1

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,14 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
  class App {
     public static void main(String[] args) {
-        String user;
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        user = Cli.user();
-        System.out.println("Hello, " + user+ "!");
+        Scanner game = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "2 - Even\n" +
+                "0 - Exit");
+        System.out.println("Your choice: ");
+        int numberGame = game.nextInt();
+        switch (numberGame) {
+            case 1:
+                Cli.hello();
+                break;
+            case 0:
+                break;
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,9 +1,14 @@
 package hexlet.code;
 import java.util.Scanner;
 
-public class Cli {
-    public static String user(){
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Cli {
+    public static void hello() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.next();
+        System.out.print("\nWelcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
     }
 }

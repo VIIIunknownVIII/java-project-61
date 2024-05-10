21fb0407dbdf179a2c2d2586f1b906bb718dc805
VIIIunknownVIII <hussarrodion@gmail.com>
2024-05-10 21:57:08 +0700

    2

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,6 +1,5 @@
 package hexlet.code;
 import java.util.Scanner;
-
 class Cli {
     public static void hello() {
         Scanner scanner = new Scanner(System.in);
@@ -8,6 +7,5 @@ class Cli {
         System.out.print("May I have your name? ");
         String user = scanner.next();
         System.out.println("Hello, " + user + "!");
-
     }
 }

d51dbd07697abff55cda22e67139b0fa5ea31331
VIIIunknownVIII <hussarrodion@gmail.com>
2024-05-11 11:14:55 +0700

    2

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -9,7 +9,8 @@ public class Engine {
         System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName = scan.next();
-        System.out.println("Hello, " + userName + "!");
+        System.out.println("Hello, "
+                + userName + "!");
 
         System.out.println(text);
 
@@ -17,16 +18,21 @@ public class Engine {
             String question = quiz[0];
             String correctAnswer = quiz[1];
 
-            System.out.println("Question: " + question);
+            System.out.println("Question: "
+                    + question);
             System.out.print("Your answer: ");
             String userAnswer = scan.next();
 
             if (userAnswer.equalsIgnoreCase(correctAnswer)) {
                 System.out.println("Correct!");
             } else {
-                System.out.println("'" + userAnswer + "' is wrong answer ;(. " +
-                        "Correct answer was '" + correctAnswer + "'.");
-                System.out.println("Let's try again, " + userName + "!");
+                System.out.println("'" + userAnswer
+                        + "' is wrong answer ;(. "
+                        +
+                        "Correct answer was '"
+                        + correctAnswer + "'.");
+                System.out.println("Let's try again, "
+                        + userName + "!");
                 return;
             }
         }

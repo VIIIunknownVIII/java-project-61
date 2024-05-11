400e57b03d3c5b512c270219446ba0e135735494
VIIIunknownVIII <hussarrodion@gmail.com>
2024-05-11 21:34:43 +0700

    2

--- app/src/main/java/hexlet/code/games/Prme.java
+++ app/src/main/java/hexlet/code/games/Prme.java
@@ -3,7 +3,7 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
-public class Prme {
+public class Prime {
     public static void run() {
         final int maxRand = 999;
         String[][] quiz = new String[Engine.ROUNDS][2];
@@ -19,16 +19,16 @@ public class Prme {
         Engine.run(inviteText, quiz);
     }
 
-    public static boolean Prime(int num) {
-        boolean Prime1 = num >= 2;
+    public static boolean Primes(int num) {
+        boolean Primes = num >= 2;
 
         for (var n = 2; n <= num / 2; n++) {
             if (num % n == 0) {
-                Prime1 = false;
+                Primes = false;
                 break;
             }
         }
 
-        return Prime1;
+        return Primes;
     }
 }

50e17d2687330eb547b1b32bbd689d50c37b9936
VIIIunknownVIII <hussarrodion@gmail.com>
2024-05-11 11:15:56 +0700

    2

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -3,8 +3,8 @@ package hexlet.code;
 import java.util.Random;
 
 public class Utils {
-    public static int generate(int StartNabor, int EndNabor) {
+    public static int generate(int Start, int End) {
         Random random = new Random();
-        return StartNabor + random.nextInt(EndNabor - StartNabor + 1);
+        return Start + random.nextInt(End - Start + 1);
     }
 }
